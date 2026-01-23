173f7a85e9926262b3c823c43e6fc0cd1761c608
ma9737149 <ahmedashraf3695@gmail.com>
2026-01-23 18:21:05 +0200

    fix some erros

--- crud_api/api/views.py
+++ crud_api/api/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView, ListAPIView
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import Product
